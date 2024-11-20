e0f218ffe5648a32727546268118a203fec0cf4d
Denys209818 <deniskravcuk63@gmail.com>
2024-11-20 02:59:13 +0200

    add close func

--- src/components/custom/Header/Header.tsx
+++ src/components/custom/Header/Header.tsx
@@ -46,6 +46,6 @@ export const Header: React.FC = () => {
             </nav>
         </div>
 
-        {menuShow && <MenuSide />}
+        {menuShow && <MenuSide close={() => setMenuShow(false)} />}
     </header>);
 }
--- src/components/custom/MenuSide/MenuSide.tsx
+++ src/components/custom/MenuSide/MenuSide.tsx
@@ -1,16 +1,20 @@
 import { Link } from "react-router-dom"
 
-export const MenuSide = () => {
+export type MenuSideType = {
+    close: () => void;
+};
+
+export const MenuSide: React.FC<MenuSideType> = ({ close }) => {
     return (<aside id="menu" className="fixed bg-[#BBEE80] z-30 size-full top-0 left-0">
         <div className="flex h-full flex-col justify-between pt-[102px] pb-[54px]">
             <nav>
                 <ul className="list-none p-0">
                     <li className="block px-2 py-[18px]">
-                        <Link to='/menu' className="block text-center font-lato text-asideMenu">Меню</Link>
+                        <Link to='/menu' onClick={close} className="block text-center font-lato text-asideMenu">Меню</Link>
                     </li>
 
                     <li className="block px-2 py-[18px]">
-                        <Link to='/' className="block text-center font-lato text-asideMenu">Кошик</Link>
+                        <Link to='/' onClick={close} className="block text-center font-lato text-asideMenu">Кошик</Link>
                     </li>
                 </ul>
             </nav>
