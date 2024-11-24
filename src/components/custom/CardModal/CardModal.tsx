c07de69f8fb3bb4f08d5cdf3fa5226e84beedd48
Denys209818 <deniskravcuk63@gmail.com>
2024-11-24 11:29:52 +0200

    remove warnings

--- src/components/custom/CardModal/CardModal.tsx
+++ src/components/custom/CardModal/CardModal.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { createPortal } from "react-dom";
 import * as Styles from '../../MainPage/styles';
